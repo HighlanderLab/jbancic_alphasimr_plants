f6f7141a40b6a4bb354ced6f1da82799c39176eb
philipbg <58154543+philipbg@users.noreply.github.com>
2023-12-12 16:22:12 +0000

    Changed Year to Stage in 112

--- 01_LineBreeding/01_PhenotypicSelection/02_SingleSeedDescent/AdvanceYear.R
+++ 01_LineBreeding/01_PhenotypicSelection/02_SingleSeedDescent/AdvanceYear.R
@@ -4,50 +4,50 @@
 #Advance breeding program by 1 year
 #Works backwards through pipeline to avoid copying data
 
-#Year 11
+#Stage 11
 #Release variety
 
-#Year 10
+#Stage 10
 EYT = selectInd(AYT, nEYT)
 EYT = setPheno(EYT, varE = varE, reps = repEYT)
 
-#Year 9
+#Stage 9
 AYT = selectInd(PYT, nAYT)
 AYT = setPheno(AYT, varE = varE, reps = repAYT)
 
-#Year 8
+#Stage 8
 output$accSel[year] = cor(HDRW@gv, HDRW@pheno)
 PYT = selectWithinFam(HDRW, famMax)
 PYT = selectInd(PYT, nPYT)
 PYT = setPheno(PYT, varE = varE, reps = repPYT)
 
-#Year 7
+#Stage 7
 HDRW = setPheno(S4, varE = varE, reps = repHDRW)
 
-#Year 6
+#Stage 6
 # Assume all seed germinates
 # Take single seed from each plant
 # Lines almost fully inbred
 S4 = self(S3)
 
-#Year 5
+#Stage 5
 # Assume all seed germinates
 # Take single seed from each plant
 S3 = self(S2)
 
-#Year 4
+#Stage 4
 # Assume all seed germinates
 # Take single seed from each plant
 S2 = self(S1)
 
-#Year 3
+#Stage 3
 # Create segregating population
 # Take single seed from each plant
 S1 = self(S0, nSelf)
 
-#Year 2
+#Stage 2
 S0 = self(F1, nSelf)
 
-#Year 1
+#Stage 1
 F1 = randCross(Parents, nCrosses)
 
