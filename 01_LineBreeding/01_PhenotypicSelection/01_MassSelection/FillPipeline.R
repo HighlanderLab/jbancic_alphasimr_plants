7c811eca1118a439f8f2ec9892fd7d5866e566ef
philipbg <58154543+philipbg@users.noreply.github.com>
2023-12-12 16:20:02 +0000

    Edited Year to Stage

--- 01_LineBreeding/01_PhenotypicSelection/01_MassSelection/FillPipeline.R
+++ 01_LineBreeding/01_PhenotypicSelection/01_MassSelection/FillPipeline.R
@@ -2,19 +2,19 @@
 # Fill breeding pipeline
 #-----------------------------------------------------------------------
 #Set initial yield trials with unique individuals
-for(year in 1:11){
-  cat("  FillPipeline year:",year,"of 11\n")
-  if(year < 3){
-    #Year 1
+for(stage in 1:2){
+  cat("  FillPipeline stage:",stage,"of 2\n")
+  if(stage < 3){
+    #Stage 1
     F1 <- randCross(Parents, nCrosses)
   }
-  if(year < 2){
-    #Year 2
+  if(stage < 2){
+    #Stage 2
     F1 <- setPheno(F1, varE = varE)
     selected <- selectInd(F1,nParents)
   }
-  if(year < 1){
-    ##Year 3
+  if(stage < 1){
+    ##Stage 3
     #Release variety
   }
 }
